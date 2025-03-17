28609bcde3e63e694d67c41cf804bdc4a2a11870
jcastrotj <jcastrotj@gmail.com>
2025-03-17 15:21:47 -0300

    Agrega lógica en js y focus en html

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-const ingresoAmigo=document.getElementById("amigo")
-const listaAmigos=[];
-const ulListaAmigos=document.getElementById("listaAmigos");
-const ulResultado=document.getElementById("resultado");
+const ingresoAmigo = document.getElementById("amigo") //Elemento nombres de amigos
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos"); //Elemento lista de amigos
+const ulResultado = document.getElementById("resultado"); //Elemento amigo sorteado
+
+function focusElemento() {
+    document.getElementById("amigo").focus();
+}
 
 function agregarAmigo(){
-    if (ingresoAmigo.value==="") {
-        alert("Por favor ingrese un nombre válido");
+    if (ingresoAmigo.value == '') {
+        alert('Por favor ingrese un nombre válido');
     } else {
         listaAmigos.push(ingresoAmigo.value);
-        ulListaAmigos.innerHTML+=`<li>${ingresoAmigo.value}</li>`;
+        ulListaAmigos.innerHTML += `<li>${ingresoAmigo.value}</li>`;
+        document.querySelector("#amigo").value = ''
+        focusElemento();
+        //console.log(listaAmigos);
     }
 }
 
 function sortearAmigo(){
-    const amigoSorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
-    ulResultado.innerHTML=`<li>El amigo secreto es: ${amigoSorteado}</li>`;
+    if (listaAmigos.length < 2) {
+        //alert('Por favor, primero ingrese los nombres de sus amigos');
+        alert(`Por favor, ${(listaAmigos.length == 0) ? 'primero ingrese los nombres de sus amigos' : 'ingrese más nombres de amigos (solo hay uno)'}`);
+        focusElemento();
+        //console.log(listaAmigos.length);
+    } else {
+        const amigoSorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+        ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSorteado}</li>`;
+        ulListaAmigos.innerHTML = '';
+        listaAmigos.splice(0);
+        focusElemento();
+        //console.log(listaAmigos);
+    }
 }
